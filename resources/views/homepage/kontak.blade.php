b5110da0cf41c6bb33f0ff29b0f613a81f50b79e
dhenoputri17 <113491152+dhenoputri17@users.noreply.github.com>
2022-10-11 09:46:20 -0700

    kontak

--- resources/views/homepage/kontak.blade.php
+++ resources/views/homepage/kontak.blade.php
@@ -1,10 +1,16 @@
 @extends('layouts.template')
 @section('content')
-<div class="container">
-  <div class="row">
-    <div class="col">
-      <h1>Kontak Kami</h1>
-    </div>
-  </div>
+<br> <br> <br>
+<div class="card mb-3 bg-dark" style="max-width: 500px; height: auto; margin: auto; color: white; border: 1px solid; border-radius: 20px;">
+      
+      <div class="card-body" style="height: auto; padding-left: 50px; padding-right: 50px;">
+        <h1 class="card-title"><center>Kontak</center></h1>
+        <br>
+        <h2 ><i class="fa-brands fa-square-instagram" style="font-size: 50px;"  >&nbsp</i><a href="#" style="text-decoration: none; color: white;"> Games_toon</a></h2>
+        <h2 ><i class="fa-brands fa-square-twitter" style="font-size: 50px;"></i>&nbsp<a href="#" style="text-decoration: none; color: white;"> @gamestoon</a> </h2>
+        <h2 ><i class="fa-brands fa-square-facebook" style="font-size: 50px;"></i>&nbsp<a href="#" style="text-decoration: none; color: white;"> GamesToon</a> </h2>
+        <h2 ><i class="fa-brands fa-square-whatsapp" style="font-size: 50px;"></i>&nbsp<a href="#" style="text-decoration: none; color: white;"> 0888222333</a> </h2>
+      </div>
+      
 </div>
 @endsection
